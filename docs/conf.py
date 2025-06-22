7c7b423225dd12a8680a9c6a70072df4f49242b4
Chong Chin Herng <chongahzong@gmail.com>
2025-06-22 19:51:43 +0800

    Add contact

--- docs/conf.py
+++ docs/conf.py
@@ -25,5 +25,38 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'furo'
 html_static_path = ['_static']
-html_css_files = ['custom.css']
+html_css_files = ['card-grid.css',
+    'contact-list.css',
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/brands.min.css"
+]
 html_title = 'Chin Herng'
+html_theme_options = {
+    "footer_icons": [
+        {
+            "name": "Email",
+            "url": "mailto:chinherng@u.nus.edu",
+            "html": "",
+            "class": "fa-solid fa-envelope fa-2x",
+        },
+        {
+            "name": "LinkedIn",
+            "url": "https://linkedin.com/in/chong-chin-herng",
+            "html": "",
+            "class": "fa-brands fa-solid fa-linkedin fa-2x",
+        },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/chin-herng",
+            "html": "",
+            "class": "fa-brands fa-solid fa-github fa-2x",
+        },
+        {
+            "name": "Instagram",
+            "url": "https://instagram.com/chongahzong",
+            "html": "",
+            "class": "fa-brands fa-solid fa-instagram fa-2x",
+        }
+    ]
+}
---
+++ docs/contact.rst
@@ -0,0 +1,25 @@
+Contact
+=======
+
+I wash my ears and listen to you respectfully.
+
+.. raw:: html
+
+    <div class="contact-list">
+        <div class="contact-item">
+            <i class="fas fa-envelope"></i>
+            <a href="mailto:chinherng@u.nus.edu">chinherng@u.nus.edu</a>
+        </div>
+        <div class="contact-item">
+            <i class="fab fa-linkedin"></i>    
+            <a href="https://linkedin.com/in/chong-chin-herng" target="_blank">linkedin.com/in/chong-chin-herng</a>
+        </div>
+        <div class="contact-item">
+            <i class="fab fa-github"></i>
+            <a href="https://github.com/chin-herng" target="_blank">github.com/chin-herng</a>
+        </div>
+        <div class="contact-item">
+            <i class="fab fa-instagram"></i>
+            <a href="https://instagram.com/chongahzong" target="_blank">@chongahzong</a>
+        </div>
+    </div>
